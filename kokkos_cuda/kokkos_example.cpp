ce06fc81f5cdffd486c4b14ad32bfd6801bf466b
sbstndb <30315405+sbstndb@users.noreply.github.com>
2024-10-28 22:42:51 +0100

    Comment dleetion

--- kokkos_cuda/kokkos_example.cpp
+++ kokkos_cuda/kokkos_example.cpp
@@ -1,32 +1,25 @@
-// kokkos_example.cpp
 #include "kokkos_example.hpp"
 #include <Kokkos_Core.hpp>
 
-// Implémentation de la fonction somme_kokkos_gpu
 int somme_kokkos(const std::vector<int>& data) {
-    // Initialiser Kokkos (et CUDA si activé)
     int somme = 0 ; 
     Kokkos::initialize();
     {
     
     size_t n = data.size();
 
-    // Allouer la vue Kokkos sur le device
     Kokkos::View<int*> device_data("device_data", n);
 
-    // Copier les données de l'hôte vers le device
     Kokkos::View<int*>::HostMirror host_data = Kokkos::create_mirror_view(device_data);
     for (size_t i = 0; i < n; ++i) {
         host_data(i) = data[i];
     }
     Kokkos::deep_copy(device_data, host_data);
 
-    // Somme parallèle sur le device (GPU)
     Kokkos::parallel_reduce(n, KOKKOS_LAMBDA(int i, int& update) {
         update += device_data(i);
     }, somme);
 
-    // Finaliser Kokkos
     }
     Kokkos::finalize();
 
--- kokkos_cuda/kokkos_example.hpp
+++ kokkos_cuda/kokkos_example.hpp
@@ -1,6 +1,4 @@
-// kokkos_example.hpp
 #include <vector>
 
-// Déclaration de la fonction qui utilise Kokkos
 int somme_kokkos(const std::vector<int>& data);
 
